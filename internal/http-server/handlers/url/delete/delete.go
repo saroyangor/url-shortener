3daf059c2480ef5e516e599e3bcf1c2b42fd257c
saroyangor <dr.saroyan98@gmail.com>
2023-10-07 16:16:22 +0400

    add delete

---
+++ internal/http-server/handlers/url/delete/delete.go
@@ -0,0 +1,61 @@
+package delete
+
+import (
+	"errors"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/render"
+	"golang.org/x/exp/slog"
+
+	resp "url-shortener/internal/lib/api/response"
+	"url-shortener/internal/lib/logger/sl"
+	"url-shortener/internal/storage"
+)
+
+//go:generate go run github.com/vektra/mockery/v2@v2.28.2 --name=URLDeleter
+type URLDeleter interface {
+	DeleteURL(alias string) error
+}
+
+func New(log *slog.Logger, urlDeleter URLDeleter) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		const op = "handlers.url.redirect.New"
+
+		log = log.With(
+			slog.String("op", op),
+			slog.String("request_id", middleware.GetReqID(r.Context())),
+		)
+
+		alias := chi.URLParam(r, "alias")
+		if alias == "" {
+			log.Info("alias is empty")
+
+			render.JSON(w, r, resp.Error("invalid request"))
+
+			return
+		}
+
+		err := urlDeleter.DeleteURL(alias)
+		if errors.Is(err, storage.ErrURLNotFound) {
+			log.Info("url not found", "alias", alias)
+
+			render.JSON(w, r, resp.Error("not found"))
+
+			return
+		}
+		if err != nil {
+			log.Error("failed to delete url", sl.Err(err))
+
+			render.JSON(w, r, resp.Error("internal error"))
+
+			return
+		}
+
+		log.Info("deleted url", slog.String("alias", alias))
+
+		// redirect to start page url
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+	}
+}
